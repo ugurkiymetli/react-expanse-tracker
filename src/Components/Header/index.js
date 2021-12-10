77fb289421537a67522b1d543c21d2229f4ef292
Ugur Kiymetli <ugurkiymetli@hotmail.com>
2021-12-10 23:15:34 +0300

    Github repo link added.

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Header() {
-  return <h2>Expense Tracker</h2>;
+  return (
+    <>
+      <h2>
+        Expense Tracker -{" "}
+        <a href="https://github.com/ugurkiymetli/expanse-tracker-react">
+          Github-Repo
+        </a>
+      </h2>
+    </>
+  );
 }
 
 export default Header;
